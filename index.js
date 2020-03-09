f0959a1320ef6fb1cc2854652f5dcf2e07196831
Alex Miller <alexmillerprojects@gmail.com>
2020-03-09 14:03:28 -0700

    able to add and get users

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+//Imports and initializing
 const express = require("express");
 const shortid = require("shortid");
 
 const server = express();
 
 server.use(express.json());
+
+//Stores users
+let users = [];
+
+//Endpoints
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "running..." });
+});
+
+server.post("/api/users", (req, res) => {
+  const newUser = req.body;
+
+  newUser.id = shortid.generate();
+
+  users.push(newUser);
+
+  res.status(201).json(newUser);
+});
+
+server.get("/api/users", (req, res) => {
+  res.status(200).json(users);
+});
 
 // Initialize Server
 const PORT = 5000;
