b4761cccb84db2a7b9ac303b8eea9d3daae9363e
Alex Miller <alexmillerprojects@gmail.com>
2020-03-09 14:16:46 -0700

    added checking for valid request

--- index.js
+++ index.js
@@ -15,13 +15,20 @@ server.get("/", (req, res) => {
 });
 
 server.post("/api/users", (req, res) => {
-  const newUser = req.body;
+  const { name, bio } = req.body;
 
-  newUser.id = shortid.generate();
+  if (name && bio) {
+    const newUser = req.body;
+    newUser.id = shortid.generate();
 
-  users.push(newUser);
+    users.push(newUser);
 
-  res.status(201).json(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide a name and bio for the user" });
+  }
 });
 
 server.get("/api/users", (req, res) => {
